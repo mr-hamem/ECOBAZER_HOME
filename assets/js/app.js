de044f92909ac8d1d5f4fc6dbdb0276b50df9322
MR HAMEM <hamemapple2018@gmail.com>
2026-02-05 23:22:38 +0600

    this is first commit

---
+++ assets/js/app.js
@@ -0,0 +1,167 @@
+$(document).ready(function () {
+  // banner slider slick js starts here
+  $('.banner_slider').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    fade: true,
+    cssEase: 'linear',
+    dots: true,
+    dotsClass: 'banner_sliderDots',
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      },
+    }
+  ]
+});
+  // banner slider slick js ends here
+  // feature section slick slider js starts here
+  $('.featureSlider').slick({
+    slidesToShow: 5,
+    autoplay: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+      }
+    ]
+  });
+  // feature section slick slider js ends here
+  // top category section slick slider starts here
+  $('.top_cat_slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: $('.leftArrow'),
+    nextArrow: $('.rightArrow'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+      }
+    ]
+  });
+  // top category section slick slider ends here
+  // countdown jquery file starts here
+  $('#cot_sale_timer').countdown('2026/04/30', function(event) {
+  var $this = $(this).html(event.strftime(''
+    +
+    '<div class="cot_sale_timer_para"><span class="span_1">%D</span><span class="span_2">Days</span></div> ' +
+    '<span class="colon_sign">:</span>'+
+    '<div class="cot_sale_timer_para"><span class="span_1">%H</span><span class="span_2">Hours</span></div> ' +
+    '<span class="colon_sign">:</span>'+
+    '<div class="cot_sale_timer_para"><span class="span_1">%M</span><span class="span_2">Min</span></div> ' +
+    '<span class="colon_sign">:</span>'+
+    '<div class="cot_sale_timer_para"><span class="span_1">%S</span><span class="span_2">Sec</span></div> ' 
+    
+    ));
+  });
+  // countdown jquery file ends here
+  // clients section slick slider starts here
+  $('.clientsReviewSlider').slick({
+    slidesToShow: 3,
+    dots: true,
+    dotsClass: 'clientsSliderDots',
+    prevArrow: $('.clientsleftArrow'),
+    nextArrow: $('.clientsrightArrow'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        },
+      }
+    ]
+  });
+  // clients section slick slider ends here
+  // venobox usage for feature product section
+  new VenoBox({
+    spinner: 'swing',
+    navigation: false,
+    navKeyboard: false,
+    navTouch: false,
+  });
+});
+let addToWishBtn = document.querySelector(`.wishList`);
+function addToWishList() { 
+  addToWishBtn.innerHTML = `<iconify-icon icon="mdi:tick"></iconify-icon>`;
+}
+addToWishBtn.addEventListener('click', addToWishList)
+// preloader section js starts here (1)
+window.onload = () => {
+  document.querySelector('.preloader').style.opacity = '0';
+  setTimeout(() => {
+    document.querySelector('.preloader').remove();
+  }, 400);
+};
+// preloader section js ends here (1)
